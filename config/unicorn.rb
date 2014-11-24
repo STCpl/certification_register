f91545126b098914731aacaf3e0c01ff370bd470
jb hewitt <jbhewitt@me.com>
2014-11-24 04:34:29 +0000

    working on capistrano + unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,21 @@
+# config/unicorn.rb for deploying with Unicorn on Heroku
+
+root = "/home/deploy/certification_register/current"
+
+working_directory root
+
+#preload_app true
+
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.certification_register.sock"
+worker_processes 2
+timeout 30
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
