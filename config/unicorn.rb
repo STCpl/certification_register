e41d7f7f5d4c656da6618d23962adb393d4a75bd
jb hewitt <jbhewitt@me.com>
2014-11-25 01:11:55 +0000

    added before_fork

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,8 @@
 # config/unicorn.rb for deploying with Unicorn on Heroku
 
+app_dir = File.expand_path('../../', __FILE__)
+#shared_dir = File.expand_path('../../../shared/', __FILE__)
+
 root = "/home/deploy/certification_register/current"
 
 working_directory root
@@ -14,8 +17,23 @@ listen "/tmp/unicorn.certification_register.sock"
 worker_processes 2
 timeout 30
 
-# Force the bundler gemfile environment variable to
-# reference the capistrano "current" symlink
-before_exec do |_|
-  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
+
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_dir}/Gemfile"
 end
